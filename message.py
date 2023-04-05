932cabade0db3cdbec338fdae7e73521064a71fd
Nicostrange707 <74682984+Nicostrange707@users.noreply.github.com>
2023-04-05 15:31:54 +0200

    Add files via upload

---
+++ message.py
@@ -0,0 +1,14 @@
+import pyautogui as pt
+import time
+
+limit = input("Anzahl der Nachrichten: ")
+message = input("Nachricht:")
+i=0
+
+time.sleep(3)
+
+while i<int(limit):
+    pt.typewrite(message)
+    pt.press("enter")
+    
+    i+=1
